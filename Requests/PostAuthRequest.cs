b9662fbaccac2deb0a74f731d65df8a9f3ab9424
paobenci@gmail.com <paobenci@gmail.com>
2022-12-17 02:49:22 +0100

    Added proper config file

--- Requests/PostAuthRequest.cs
+++ Requests/PostAuthRequest.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
 using RestSharp;
+using System.Configuration;
 
 namespace Cappario
 {
     public class PostAuthRequest
     {
-        public string EndpointUrl { get; } = Config.ApiBaseUrl + "/auth/backend";
+        public string EndpointUrl { get; } = ConfigurationManager.AppSettings.Get("ApiBaseUrl") + "/auth/backend";
         public string Token { get; private set; }
 
         public void GetToken()
@@ -14,10 +15,10 @@ namespace Cappario
             RestRequest Request = new RestRequest("request/oauth") { Method = Method.POST };
             Request.AddHeader("Accept", "application/json");
             Request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            Request.AddParameter("client_id", Config.ClientId);
-            Request.AddParameter("client_secret", Config.ClientSecret);
-            Request.AddParameter("username", Config.BackendUsername);
-            Request.AddParameter("password", Config.BackendPassword);
+            Request.AddParameter("client_id", ConfigurationManager.AppSettings.Get("ClientId"));
+            Request.AddParameter("client_secret", ConfigurationManager.AppSettings.Get("ClientSecret"));
+            Request.AddParameter("username", ConfigurationManager.AppSettings.Get("BackendUsername"));
+            Request.AddParameter("password", ConfigurationManager.AppSettings.Get("BackendPassword"));
             Request.AddParameter("grant_type", "client_credentials");
             IRestResponse Response = Client.Execute(Request);
             Token = JsonConvert.DeserializeObject<dynamic>(Response.Content)["access_token"];
