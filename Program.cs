bb57f99c7bcd199f00f2cff81d196057d1b1a896
paobenci@gmail.com <paobenci@gmail.com>
2023-01-02 13:27:26 +0100

    Needed to reset browser for each contract

--- Program.cs
+++ Program.cs
@@ -13,22 +13,21 @@ namespace Cappario
             GetIntegrationContractsRequest.GetContractsToCheck();
             var GetContractsRequest = new GetContractsRequest(PostAuthRequest.Token, GetIntegrationContractsRequest.ListOfIdOfContractsToCheck);
             GetContractsRequest.GetCodeOfContractsThatNeedBranchChange();
-            IWebDriver Driver = new Browsers().LaunchChrome();
-            Driver.Manage().Window.Maximize();
-            var LoginPage = new LoginPage(Driver);
-            LoginPage.GoToUrl();
-            LoginPage.Login();
-            var RentalsPage = new RentalsPage(Driver);
             foreach (Contract Contract in GetContractsRequest.ListOfCodeOfContractsThatNeedBranchChange)
             {
+                IWebDriver Driver = new Browsers().LaunchChrome();
+                Driver.Manage().Window.Maximize();
+                var LoginPage = new LoginPage(Driver);
+                LoginPage.GoToUrl();
+                LoginPage.Login();
+                var RentalsPage = new RentalsPage(Driver);
                 RentalsPage.GoTo();
                 RentalsPage.SearchContractByCode(Contract.Code);
                 var RentalsDetailsPage = new RentalsDetailsPage(Driver);
                 RentalsDetailsPage.GoTo();
                 RentalsDetailsPage.EditFiscalBranch(Contract.Code, Contract.RightFiscalBranch);
-                RentalsDetailsPage.CloseAllTabs();
+                Driver.Quit();
             }
-            Driver.Quit();
         }
     }
 }
