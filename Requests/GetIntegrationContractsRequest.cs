accc54799a263426bd2f88b158aa9fccc5db7af4
paobenci@gmail.com <paobenci@gmail.com>
2023-01-04 16:20:39 +0100

    Added pagination logic

--- Requests/GetIntegrationContractsRequest.cs
+++ Requests/GetIntegrationContractsRequest.cs
@@ -19,24 +19,31 @@ namespace Cappario
             this.Token = Token;
         }
 
-        public void SendRequest()
+        private void SendRequest(int Offset)
         {
             RestClient Client = new RestClient(EndpointUrl);
             RestRequest Request = new RestRequest(Method.GET);
             Request.AddParameter("Authorization", "Bearer " + Token, ParameterType.HttpHeader);
             Request.AddParameter("creation_date_from", FromCreationDate);
             Request.AddParameter("status", "approved,running");
+            Request.AddParameter("offset", Offset);
             DeserializedJson = JsonConvert.DeserializeObject<dynamic>(Client.Execute(Request).Content);
         }
 
         public List<string> GetContractsToCheck()
         {
-            SendRequest();
-            var contracts = DeserializedJson["contracts"].Count;
-            for (int e = 0; e < contracts; e++)
+            var Offset = 0;
+            SendRequest(Offset);
+            while (DeserializedJson["next"] != null)
             {
-                ListOfIdOfContractsToCheck.Add(Convert.ToString(DeserializedJson["contracts"][e]["id"]));
-                Console.WriteLine("Added contract " + DeserializedJson["contracts"][e]["code"] + " to the list of contracts that must be checked");
+                SendRequest(Offset);
+                Offset+=100;
+                var contracts = DeserializedJson["contracts"].Count;
+                for (int e = 0; e < contracts; e++)
+                {
+                    ListOfIdOfContractsToCheck.Add(Convert.ToString(DeserializedJson["contracts"][e]["id"]));
+                    Console.WriteLine("Added contract " + DeserializedJson["contracts"][e]["code"] + " to the list of contracts that must be checked");
+                }
             }
             return ListOfIdOfContractsToCheck;
         }
